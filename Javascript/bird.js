df1e60aa90a926a1d499b1ad56c68b4391fe984b
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2020-02-24 21:12:15 +0100

    Set bird droping speed and position

--- Javascript/bird.js
+++ Javascript/bird.js
@@ -1,79 +1,91 @@
-var bird = function(game){
+var bird = function (game) {
     this.game = game;
     this.images = [];
-    this.loaded = [false,false,false];
+    this.loaded = [false, false, false];
 
-    this.currentBirdImage = null;    
+    this.currentBirdImage = null;
     this.currentFrame = 0;
+    this.birdPos = 0;
+    this.birdSpeed = 0;
+    this.birdAcceleration = 0.5;
 
     self = this;
 
 
-    this.init = function(){
+    this.init = function () {
         this.loadImages();
     }
 
-    this.loadImages = function(){
+    this.loadImages = function () {
         var imgBird = new Image();
         var imgBirdDown = new Image();
         var imgBirdUp = new Image();
 
-        imgBird.onload = function(){
+        imgBird.onload = function () {
             self.loaded[0] = true;
             self.currentBirdImage = imgBird;
             self.images.push(imgBird);
         }
 
-        imgBirdUp.onload = function(){
+        imgBirdUp.onload = function () {
             self.loaded[1] = true;
             self.images.push(imgBirdUp);
         }
 
-        imgBirdDown.onload = function(){
-            self.loaded[2] =true;
+        imgBirdDown.onload = function () {
+            self.loaded[2] = true;
             self.images.push(imgBirdDown);
-        }      
-        
+        }
+
         //Load all the birds
         imgBird.src = 'sprites/bird.png';
         imgBirdUp.src = 'sprites/birdup.png';
         imgBirdDown.src = 'sprites/birddown.png';
 
-        
-        
-        
+
+
+
     }
 
-    this.update = function(){
+    this.update = function () {
         //Check if all Images are loaded
-        if(!self.loaded[0] || !self.loaded[1] || !self.loaded[2]){
+        if (!self.loaded[0] || !self.loaded[1] || !self.loaded[2]) {
             return;
         }
 
+        //Flappy bird animation
         self.currentFrame++;
-        if(self.currentFrame == 30){
+        if (self.currentFrame == 16) {
             self.currentFrame = 0;
         }
-
         this.changeImage(self.currentFrame);
+
+        //Adjust the bird position
+        if(this.birdPos <= 480){
+            this.birdSpeed += this.birdAcceleration;
+            this.birdPos += this.birdSpeed * 1;
+        }
+        else{
+            this.birdPos = 488;
+        }
         
+
         //console.log('bird update');
     }
 
-    this.changeImage = function(frame){
-        if(frame == 9){
+    this.changeImage = function (frame) {
+        if (frame == 5) {
             self.currentBirdImage = self.images[0];
-        }
-        else if(frame == 19){
+        } else if (frame == 10) {
             self.currentBirdImage = self.images[1];
-        }else if(frame == 29){
+        } else if (frame == 15) {
             self.currentBirdImage = self.images[2];
         }
     }
 
-    this.draw = function(){
-        if(self.loaded[0] && self.loaded[1] && self.loaded[2]){
-            self.game.context.drawImage(self.currentBirdImage, 100, 100);
+    this.draw = function () {
+        if (self.loaded[0] && self.loaded[1] && self.loaded[2]) {
+            self.game.context.drawImage(self.currentBirdImage, 100, this.birdPos);
             //console.log('bird drawn');
         }
     }
